b0b89c6a0bc66cf5c503f852d96b1643d1836555
林昌賜 <brian29688071@gmail.com>
2020-11-08 22:03:14 +0800

    sql的名稱變換

--- database/migrations/2020_11_08_134403_team06_typhoon.php
+++ database/migrations/2020_11_08_134403_team06_typhoon.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Team06Typhoon extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('team06_typhoon', function (Blueprint $table) {
+            $table->integer("year");
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->boolean('arrived');
+            $table->integer('powerLV');
+            $table->integer('level');
+            $table->foreign('powerLV')->references('id')->on('levels');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('team06_typhoon');
     }
 }
